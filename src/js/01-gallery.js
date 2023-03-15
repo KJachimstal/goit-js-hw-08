6554a174ef95118fd649e8e5c3018aaa99a6c700
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-15 19:18:18 +0100

    Completed first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('div.gallery');
+
+for (const image of galleryItems) {
+  const galleryItem = document.createElement('a');
+  galleryItem.classList.add('gallery__item');
+  galleryItem.href = image.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = image.preview;
+  img.alt = image.description;
+
+  galleryItem.insertAdjacentElement('afterbegin', img);
+  gallery.insertAdjacentElement('afterbegin', galleryItem);
+}
+
+gallery.addEventListener('click', onImageClick);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  lightbox.on('show.simplelightbox');
+}
