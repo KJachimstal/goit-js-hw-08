e43cbce9d56324e4f3fc21b68e543b3c42edc93b
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-17 00:15:34 +0100

    Completed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player
+  .setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+  )
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        console.log(error.name);
+        break;
+
+      default:
+        // some other error occurred
+        console.log(error.name);
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
