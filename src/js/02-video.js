788de4c11a7c800f284142ace3ec5a65174d3f22
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-17 09:06:07 +0100

    Corrected time in second tesk

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,10 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const currentTimeData =
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || {};
 player
-  .setCurrentTime(
-    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
-  )
+  .setCurrentTime(currentTimeData.seconds || 0)
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
